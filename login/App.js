147e8ddd127a00f56356fe38fa73df8f37fdf8dc
Sheetal Jaybhay <sheetaljaybhay04@gmail.com>
2018-03-22 21:14:52 +0530

    Storage bucket added and rearranged the imports

--- login/App.js
+++ login/App.js
@@ -5,20 +5,20 @@ import {
 } from 'react-native';
 
 
+
+import Routes from './src/Routes';
+import * as firebase from "firebase";
+
 const config = {
     apiKey: "AIzaSyCFO8yZNMOl80GSa7mKpAvYtf4IBc2wQFc",
     authDomain: "login-11519.firebaseapp.com",
     databaseURL: "https://login-11519.firebaseio.com",
     projectId: "login-11519",
-    storageBucket: "",
+    storageBucket: "login-11519.appspot.com",
     messagingSenderId: "520734790523"
 };
 
 
-
-
-import Routes from './src/Routes';
-import * as firebase from "firebase";
 
 export default class App extends React.Component {
 
