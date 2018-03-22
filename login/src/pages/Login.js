142d698fbc93b902dc327700fc3e5eb53f32e8fe
Sheetal Jaybhay <sheetaljaybhay04@gmail.com>
2018-03-22 20:57:47 +0530

    Fixed a single quote error

--- login/src/pages/Login.js
+++ login/src/pages/Login.js
@@ -59,7 +59,7 @@ export default class Login extends React.Component {
                 </TouchableOpacity>
 
                 <View style={styles.signupTextCont}>
-                    <Text style={styles.signupText}>Don't have an account yet?</Text>
+                    <Text style={styles.signupText}>New User?</Text>
                     <TouchableOpacity onPress={this.signup}><Text style={styles.signupButton}> Signup</Text></TouchableOpacity>
                 </View>
             </View>
